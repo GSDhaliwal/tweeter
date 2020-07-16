1cd6d955b8d5a0a5545077568537e554b2181568
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-16 00:16:15 +0000

    Fetch tweets with Ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,31 +6,6 @@
 
 $(document).ready(function() {
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       const $tweet = createTweetElement(tweet);
@@ -61,20 +36,28 @@ $(document).ready(function() {
     return $(stringifiedTweet);
   }
 
-  renderTweets(data);
-
   $('#form').submit(function(event) {
     event.preventDefault();
     $.ajax({
       url: '/tweets/',
-      method: "POST", 
+      method: 'POST', 
       data: $(this).serialize()
     })
     .then(function(res) {
       console.log('Success: ', res);
+      loadTweets(res);
     })
-})
- 
-  
+  })
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets', 
+      method: 'GET',
+    })
+    .then(function(res) {
+      $('#tweets-container').empty();
+      renderTweets(res);
+    })
+  }
 
 });
